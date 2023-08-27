921e8e6e31522a3a4341b86cbc4fc56d8a8e38b6
tal0311 <tal.amit0311@gmail.com>
2023-08-27 20:01:55 +0300

    router file to handle navigation

---
+++ js/router.js
@@ -0,0 +1,52 @@
+function navigateTo(route = 'home') {
+ const pages = [...document.querySelectorAll('.page')]
+ const elBody = document.querySelector('body')
+ elBody.setAttribute('page', route)
+ pages.forEach((page) => {
+  !page.classList.contains(route)
+   ? (page.hidden = true)
+   : (page.hidden = false)
+ })
+
+ renderBy(route)
+ setActiveClass(route)
+ setTitleByRoute(route)
+}
+
+function setTitleByRoute(route) {
+ document.title = route.substring(0, 1).toUpperCase() + route.substring(1)
+}
+
+function setActiveClass(route) {
+
+ const elLinks = document.querySelectorAll('nav li')
+ elLinks.forEach((link) => {
+  const linkName = link.getAttribute('name')
+  link.classList.remove('active')
+  if (linkName === route) link.classList.add('active')
+ })
+}
+
+function renderBy(route) {
+ switch (route) {
+  case 'user':
+   setDocTitleAndIcon('user', 'User Preferences')
+   initUser()
+   break
+  case 'map':
+   // in Map controller
+   setDocTitleAndIcon('map', 'Map')
+   initMap()
+   break
+  case 'home':
+   setDocTitleAndIcon('home', 'Home')
+  // in Home controller
+  default:
+   break
+ }
+}
+
+function setDocTitleAndIcon(prop, val) {
+ document.title = val
+ document.querySelector(`link[rel*="icon"]`).href = `/assets/images/${prop}.svg`
+}
